f3c5bb125c7a370217fc8f4358448c16e055cb45
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:16:45 +0530

    Update pause.py

--- BTSchinna/plugins/admins/pause.py
+++ BTSchinna/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from BTSchinna import app
+from BTSchinna.core.call import Anony
+from BTSchinna.utils.database import is_music_playing, music_off
+from BTSchinna.utils.decorators import AdminRightsCheck
+from BTSchinna.utils.inline import close_markup
 from config import BANNED_USERS
 
 
