3475781fda1fb736cfa21d8ff2a61b0ecc023ff4
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:14:09 +0530

    Update callback.py

--- BTSchinna/plugins/admins/callback.py
+++ BTSchinna/plugins/admins/callback.py
@@ -3,10 +3,10 @@ import asyncio
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import SUDOERS, db
-from AnonXMusic.utils.database import (
+from BTSchinna import YouTube, app
+from BTSchinna.core.call import Anony
+from BTSchinna.misc import SUDOERS, db
+from BTSchinna.utils.database import (
     get_active_chats,
     get_lang,
     get_upvote_count,
@@ -17,11 +17,11 @@ from AnonXMusic.utils.database import (
     music_on,
     set_loop,
 )
-from AnonXMusic.utils.decorators.language import languageCB
-from AnonXMusic.utils.formatters import seconds_to_min
-from AnonXMusic.utils.inline import close_markup, stream_markup, stream_markup_timer
-from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
+from BTSchinna.utils.decorators.language import languageCB
+from BTSchinna.utils.formatters import seconds_to_min
+from BTSchinna.utils.inline import close_markup, stream_markup, stream_markup_timer
+from BTSchinna.utils.stream.autoclear import auto_clean
+from BTSchinna.utils.thumbnails import get_thumb
 from config import (
     BANNED_USERS,
     SUPPORT_CHAT,
