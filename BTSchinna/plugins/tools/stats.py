30c25b421754db1e3ef62f5f041403d492fbe18e
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 18:16:00 +0530

    Update stats.py

--- BTSchinna/plugins/tools/stats.py
+++ BTSchinna/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from AnonXMusic import app
-from AnonXMusic.core.userbot import assistants
-from AnonXMusic.misc import SUDOERS, mongodb
-from AnonXMusic.plugins import ALL_MODULES
-from AnonXMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from AnonXMusic.utils.decorators.language import language, languageCB
-from AnonXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from BTSchinna import app
+from BTSchinna.core.userbot import assistants
+from BTSchinna.misc import SUDOERS, mongodb
+from BTSchinna.plugins import ALL_MODULES
+from BTSchinna.utils.database import get_served_chats, get_served_users, get_sudoers
+from BTSchinna.utils.decorators.language import language, languageCB
+from BTSchinna.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
