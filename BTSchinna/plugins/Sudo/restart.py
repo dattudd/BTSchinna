52d02da8bc9ff05067cbc2611b0f4d78c1605df0
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:10:34 +0530

    Update restart.py

--- BTSchinna/plugins/Sudo/restart.py
+++ BTSchinna/plugins/Sudo/restart.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_sudo, remove_sudo
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.extraction import extract_user
-from AnonXMusic.utils.inline import close_markup
+from BTSchinna import app
+from BTSchinna.misc import SUDOERS
+from BTSchinna.utils.database import add_sudo, remove_sudo
+from BTSchinna.utils.decorators.language import language
+from BTSchinna.utils.extraction import extract_user
+from BTSchinna.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
