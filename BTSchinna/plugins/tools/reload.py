c2b3cb62d81ddf6e3657fcb040d8b925cc922264
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 18:13:07 +0530

    Update reload.py

--- BTSchinna/plugins/tools/reload.py
+++ BTSchinna/plugins/tools/reload.py
@@ -5,12 +5,12 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import CallbackQuery, Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import get_assistant, get_authuser_names, get_cmode
-from AnonXMusic.utils.decorators import ActualAdminCB, AdminActual, language
-from AnonXMusic.utils.formatters import alpha_to_int, get_readable_time
+from BTSchinna import app
+from BTSchinna.core.call import Anony
+from BTSchinna.misc import db
+from BTSchinna.utils.database import get_assistant, get_authuser_names, get_cmode
+from BTSchinna.utils.decorators import ActualAdminCB, AdminActual, language
+from BTSchinna.utils.formatters import alpha_to_int, get_readable_time
 from config import BANNED_USERS, adminlist, lyrical
 
 rel = {}
