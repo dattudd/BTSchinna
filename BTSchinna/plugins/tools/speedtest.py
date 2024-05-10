009fba004f7ae69e1cf36fc3ddb268dd3975fe72
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 18:13:55 +0530

    Update speedtest.py

--- BTSchinna/plugins/tools/speedtest.py
+++ BTSchinna/plugins/tools/speedtest.py
@@ -4,9 +4,9 @@ import speedtest
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.decorators.language import language
+from BTSchinna import app
+from BTSchinna.misc import SUDOERS
+from BTSchinna.utils.decorators.language import language
 
 
 def testspeed(m, _):
