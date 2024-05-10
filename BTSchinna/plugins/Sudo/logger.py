358bb7e8bcdc38bbb37b0f5bdfbd89c845ae3662
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:08:47 +0530

    Update logger.py

--- BTSchinna/plugins/Sudo/logger.py
+++ BTSchinna/plugins/Sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from BTSchinna import app
+from BTSchinna.misc import SUDOERS
+from BTSchinna.utils.database import add_off, add_on
+from BTSchinna.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
