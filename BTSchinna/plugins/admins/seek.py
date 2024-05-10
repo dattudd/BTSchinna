fc90b0aabc72857e97b2b0688e002de51aa8bfd5
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:47:17 +0530

    Update seek.py

--- BTSchinna/plugins/admins/seek.py
+++ BTSchinna/plugins/admins/seek.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils import AdminRightsCheck, seconds_to_min
-from AnonXMusic.utils.inline import close_markup
+from BTSchinna import YouTube, app
+from BTSchinna.core.call import Anony
+from BTSchinna.misc import db
+from BTSchinna.utils import AdminRightsCheck, seconds_to_min
+from BTSchinna.utils.inline import close_markup
 from config import BANNED_USERS
 
 
