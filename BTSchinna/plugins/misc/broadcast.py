86ab79ff2dd5ea042467bd779b4262e24f5201f5
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-05-10 17:58:13 +0530

    Update broadcast.py

--- BTSchinna/plugins/misc/broadcast.py
+++ BTSchinna/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import (
+from BTSchinna import app
+from BTSchinna.misc import SUDOERS
+from BTSchinna.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.formatters import alpha_to_int
+from BTSchinna.utils.decorators.language import language
+from BTSchinna.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
